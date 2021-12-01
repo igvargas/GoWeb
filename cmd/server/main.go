d7854f724c5bde51cd7058d266bab9c856c51c12
igvargas <ignacio.vargas@mercadolibre.com>
2021-12-01 00:00:23 -0300

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	usuarios "github.com/extmatperez/meli_bootcamp2/7_goweb2/internal/usuarios"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	repo := usuarios.NewRepository()
+	service := usuarios.NewService(repo)
+	controller := usuarios.NewUsuario(service)
+
+	router.GET("/usuarios/get", controller.GetAll())
+	router.POST("/usuarios/add", controller.Store())
+
+	router.Run(":8000")
+}
