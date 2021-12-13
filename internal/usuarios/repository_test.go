6205743a7189f673532c393af1cc4bf70a277eda
igvargas <ignacio.vargas@mercadolibre.com>
2021-12-13 14:55:45 -0300

    add test

---
+++ internal/usuarios/repository_test.go
@@ -0,0 +1,90 @@
+package internal
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var usr string = `[
+	{"id": 1,"nombre": "josefa","apellido": "Maylor","email": "lmaylor0@dagondesign.com","edad": 30,"altura": 177.1,"activo": false,"fecha_creacion": "8/20/2021"},
+    {"id": 2,"nombre": "Lenard","apellido": "Maylor","email": "lmaylor0@dagondesign.com","edad": 30,"altura": 177.1,"activo": false,"fecha_creacion": "8/20/2021"}]`
+
+type StubStore struct {
+	useUpdateNombre bool
+}
+
+type MockStore struct {
+	useUpdateNombre bool
+}
+
+func (s *StubStore) Read(data interface{}) error {
+	return json.Unmarshal([]byte(usr), &data)
+
+}
+
+func (s *StubStore) Write(data interface{}) error {
+	return nil
+}
+
+func (m *MockStore) Read(data interface{}) error {
+	m.useUpdateNombre = true
+	return json.Unmarshal([]byte(usr), &data)
+
+}
+
+func (m *MockStore) Write(data interface{}) error {
+	return nil
+}
+
+func TestGetAll(t *testing.T) {
+	// Arrange
+	store := StubStore{}
+	repo := NewRepository(&store)
+
+	// Act
+	users, _ := repo.GetAll()
+	var expected []Usuario
+	json.Unmarshal([]byte(usr), &expected)
+
+	// Assert
+	assert.Equal(t, expected, users)
+}
+
+func TestUpdateName(t *testing.T) {
+	// Arrange
+	store := MockStore{}
+	repo := NewRepository(&store)
+
+	nombreExpected := "Pepe"
+
+	// Act
+	userUpdated, _ := repo.UpdateNombre(1, "Pepe")
+
+	// Assert
+	assert.Equal(t, nombreExpected, userUpdated.Nombre)
+	assert.True(t, store.useUpdateNombre)
+}
+
+func TestUpdateNameError(t *testing.T) {
+	// Arrange
+	store := MockStore{}
+	repo := NewRepository(&store)
+
+	// Act
+	_, err := repo.UpdateNombre(4, "Pepe")
+
+	// Assert
+	assert.Error(t, err)
+}
+
+// func TestUpdateNameRead(t *testing.T) {
+// 	// Arrange
+// 	store := MockStore{}
+
+// 	// Act
+
+// 	// Assert
+// 	assert.True(t, store.useUpdateNombre)
+// }
---
+++ internal/usuarios/service_test.go
@@ -0,0 +1,33 @@
+package internal
+
+// import "testing"
+
+// //type MockRepository struct{}
+
+// var user string = `[
+// 	{"id": 1,"nombre": "josefa","apellido": "Maylor","email": "lmaylor0@dagondesign.com","edad": 30,"altura": 177.1,"activo": false,"fecha_creacion": "8/20/2021"},
+//     {"id": 2,"nombre": "Lenard","apellido": "Maylor","email": "lmaylor0@dagondesign.com","edad": 30,"altura": 177.1,"activo": false,"fecha_creacion": "8/20/2021"}]`
+
+// func (m *MockRepository) GetAll() ([]Usuario, error) {
+// 	return []Usuario{}, nil
+// }
+// func (m *MockRepository) Store(id int, nombre string, apellido string, email string, edad int, altura float64, activo bool, fecha_creacion string) (Usuario, error) {
+// 	return Usuario{}, nil
+// }
+// func (m *MockRepository) LastId() (int, error) {
+// 	return 0, nil
+// }
+// func (m *MockRepository) Update(id int, nombre, apellido string, email string, edad int, altura float64, activo bool, fecha_creacion string) (Usuario, error) {
+// 	return Usuario{}, nil
+// }
+// func (m *MockRepository) UpdateNombre(id int, nombre string) (Usuario, error) {
+
+// 	return Usuario{}, nil
+// }
+// func (m *MockRepository) Delete(id int) error {
+// 	return nil
+// }
+
+// func TestUpdateName(t *testing.T) {
+
+// }
